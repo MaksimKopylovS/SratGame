58148877fd2dae468fbe896f87bf3e2122937a1a
Maksim <maksim.kopylov.x5@gmail.com>
2021-10-12 22:39:52 +0300

    Initial commit

---
+++ desktop/src/max_sk/star/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package max_sk.star.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import max_sk.star.game.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
