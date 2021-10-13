78a7b7c0bd67032c54928ef0c758b829884e5977
Maksim <maksim.kopylov.x5@gmail.com>
2021-10-13 21:35:15 +0300

    add backGroud Texture

--- core/src/max_sk/star/game/StarGame.java
+++ core/src/max_sk/star/game/StarGame.java
@@ -1,31 +1,34 @@
 package max_sk.star.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	SpriteBatch spriteBatch;
+	Texture seaTexture;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		spriteBatch = new SpriteBatch();
+		seaTexture = new Texture("sea.jpg");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		spriteBatch.begin();
+		spriteBatch.draw(seaTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		spriteBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		spriteBatch.dispose();
+		seaTexture.dispose();
 	}
 }
