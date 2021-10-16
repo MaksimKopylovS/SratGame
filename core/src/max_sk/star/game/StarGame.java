256a55a5030ca62a4595e4e5bbee733bf5196b49
Maksim <maksim.kopylov.x5@gmail.com>
2021-10-16 22:20:52 +0300

    Vectors and InputProcessor события нажатия

--- core/src/max_sk/star/game/StarGame.java
+++ core/src/max_sk/star/game/StarGame.java
@@ -1,34 +1,21 @@
 package max_sk.star.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch spriteBatch;
-	Texture seaTexture;
+import max_sk.star.game.screen.MenuScreen;
+import max_sk.star.game.vector.Vector;
 
-	
+public class StarGame extends Game {
+
 	@Override
 	public void create () {
-		spriteBatch = new SpriteBatch();
-		seaTexture = new Texture("sea.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		spriteBatch.begin();
-		spriteBatch.draw(seaTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		spriteBatch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		spriteBatch.dispose();
-		seaTexture.dispose();
+//		Вданный метод передаётся любой клас наследник от Screan
+		setScreen(new MenuScreen());
 	}
 }
