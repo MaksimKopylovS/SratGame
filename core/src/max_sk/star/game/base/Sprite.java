d48fb6ec11a7c0e905298c3da1b11e624ab7e965
Maksim <maksim.kopylov.x5@gmail.com>
2021-10-27 22:08:11 +0300

    HomeWorkFive

--- core/src/max_sk/star/game/base/Sprite.java
+++ core/src/max_sk/star/game/base/Sprite.java
@@ -1,10 +1,12 @@
 package max_sk.star.game.base;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import max_sk.star.game.math.Rect;
+import max_sk.star.game.util.Regions;
 
 
 public class Sprite extends Rect {
@@ -13,6 +15,11 @@ public class Sprite extends Rect {
     protected float scale = 1f;
     protected TextureRegion[] regions;
     protected int frame;
+    private boolean destryed;
+
+    public Sprite(){
+
+    }
 
     public Sprite(TextureRegion region) {
         if (region == null) {
@@ -22,11 +29,17 @@ public class Sprite extends Rect {
         regions[0] = region;
     }
 
+    public Sprite (TextureRegion region, int rows, int cols, int frames) {
+        this.regions = Regions.split(region, rows, cols, frames);
+    }
+
     public void setHeightProportion(float height) {
         setHeight(height);
         float aspect = regions[frame].getRegionWidth() / (float) regions[frame].getRegionHeight();
         setWidth(height * aspect);
     }
+
+
 
     public void resize(Rect worldBounds) {
 
@@ -74,5 +87,17 @@ public class Sprite extends Rect {
 
     public void setScale(float scale) {
         this.scale = scale;
+    }
+
+    public boolean isDestroyed(){
+        return destryed;
+    }
+
+    public void flushDestroy(){
+        destryed = false;
+    }
+
+    public void destroy(){
+        destryed = true;
     }
 }
